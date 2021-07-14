43e6fa166f7e7bd625bfbc8ce22316d08405b151
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-07-14 16:33:58 +0900

    Added query logic for getLesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -7,14 +7,12 @@ export class LessonResolver {
   constructor(
     private lessonService: LessonService
   ) {}
+
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'random',
-      name: 'Physics Class',
-      startDate: (new Date()).toISOString(),
-      endDate: (new Date()).toISOString(),
-    };
+  lesson(
+    @Args('id') id: string,
+  ) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(returns => LessonType)

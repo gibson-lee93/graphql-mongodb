0bb78327b3fe1226ea258f197f91ebd9be15552b
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-07-18 23:31:45 +0900

    Added createStudent

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,18 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+
+@Resolver(of => StudentType)
+export class StudentResolver {
+  constructor(
+    private studentService: StudentService
+  ) {}
+
+  @Mutation(returns => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
